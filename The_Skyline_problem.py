c4c13e7b26285876e0d9758a0a70b76c6805e431
Anushka Shrivastava <146864240+anushka157@users.noreply.github.com>
2024-10-26 23:43:07 +0530

    The_Skyline_problem.py

---
+++ The_Skyline_problem.py
@@ -0,0 +1,26 @@
+from sortedcontainers import SortedList 
+
+class Solution:
+    def getSkyline(self, buildings):
+        dict1 = defaultdict(list)
+
+        for start,end,height in buildings:
+            dict1[start].append([1,height])
+            dict1[end].append([-1,height])
+
+        res, prev, ans = [], -1, SortedList()
+
+        ans.add(0)
+
+        for i in sorted(dict1.keys()):
+            for f,h in dict1[i]:
+                if f == -1:
+                    ans.remove(h)
+                else:
+                    ans.add(h)
+
+            if ans[-1] != prev:
+                prev = ans[-1]
+                res.append([i,ans[-1]])
+
+        return res 
